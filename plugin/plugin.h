4a97203234349c708439eecbad42aa60b920872a
Chris Holland <zrenfire@gmail.com>
2020-06-23 21:52:21 -0400

    Fix suggest override

--- plugin/plugin.h
+++ plugin/plugin.h
@@ -8,7 +8,7 @@ class Plugin : public QQmlExtensionPlugin
     Q_OBJECT
     Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QQmlExtensionInterface")
 public:
-    virtual void registerTypes(const char * uri);
+    void registerTypes(const char * uri) override;
 };
 
 #endif // PLUGIN_H
